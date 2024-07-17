c84a1a6b7b25cb8ab0aa0381cf4ca84c6061ec15
sebastiao lopes <lopescastroneto@gmail.com>
2024-07-17 00:53:17 -0300

    finalizando o projeto, adicionando paginas

--- pages/3_⚽teams.py
+++ pages/3_⚽teams.py
@@ -1,1 +1,35 @@
 import streamlit as st
+
+st.set_page_config(
+    page_title="Players",
+    layout="wide"
+)
+
+df_data = st.session_state["data"] # carregando o estado do dataset da outra pagina
+
+clubes = df_data["Club"].unique() # carregando os valores unicos da coluna club
+club = st.sidebar.selectbox("Clube", clubes) # incluindo um filtro selectbox dentro do sidebar pegando so o resultado de clubes
+
+df_filtered = df_data[df_data["Club"] == club].set_index("Name")
+
+st.image(df_filtered.iloc[0]["Club Logo"])
+st.markdown(f"## {club}")
+
+columns = ["Age", "Photo", "Flag", "Overall", "Value(£)", 
+           "Wage(£)", "Joined", "Height(cm.)", "Weight(lbs.)", 
+           "Contract Valid Until", "Release Clause(£)"]
+
+st.dataframe(df_filtered[columns],
+             column_config={
+                 "Overall": st.column_config.ProgressColumn(
+                     "Overall", format="%d", min_value=0, max_value=100
+                 ),
+
+                 "Wage(£)": st.column_config.ProgressColumn("Weekly Wage", format="£%f", min_value=0, max_value=df_filtered["Wage(£)"].max()),
+                 "Photo": st.column_config.ImageColumn(),
+                 "Flag": st.column_config.ImageColumn("Country")
+
+             }
+             
+             )
+
